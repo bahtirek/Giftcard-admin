d5a4cf63ea6e4d0cfb8e4381ab416c77a4c9ad20
Bahti rek <bahtirek@gmail.com>
2026-06-05 23:40:46 -0400

    clean up

--- src/app/dashboard/account/account-form/account-form.ts
+++ src/app/dashboard/account/account-form/account-form.ts
@@ -1,12 +1,11 @@
 import { Component, effect, signal } from '@angular/core';
 import { Account, accountSchema, initialAccountData } from './account-form-interface';
-import { email, form, FormField, minLength, required } from '@angular/forms/signals';
-import { NgClass } from '@angular/common';
+import { form, FormField } from '@angular/forms/signals';
 import { CustomInput } from "../../../common/custom-input/custom-input";
 
 @Component({
   selector: 'account-form',
-  imports: [FormField, NgClass, CustomInput],
+  imports: [FormField, CustomInput],
   templateUrl: './account-form.html',
   styleUrl: './account-form.scss',
 })
