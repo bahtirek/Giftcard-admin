55143bb4ddb31f8685957edb388d88d22866f72d
Bahti rek <bahtirek@gmail.com>
2026-06-06 12:32:54 -0400

    replaced custom-inpit by app-input

--- src/app/common/forms/input/input.ts
+++ src/app/common/forms/input/input.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { uniqueId } from '../../../helpers/uniqueid';
+import { FormField } from '@angular/forms/signals';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [FormField, NgClass],
   templateUrl: './input.html',
   styleUrl: './input.scss',
 })
-export class Input {}
+export class Input {
+  inputFormField = input<any>();
+  required = input<boolean>(false);
+  label = input<string>('');
+  placeholder = input<string>('');
+  type = input<string>('text');
+  id = signal<string>(uniqueId());
+}
--- src/app/dashboard/account/account-form/account-form.ts
+++ src/app/dashboard/account/account-form/account-form.ts
@@ -1,11 +1,11 @@
 import { Component, effect, signal } from '@angular/core';
 import { Account, accountSchema, initialAccountData } from './account-form-interface';
 import { form, FormField } from '@angular/forms/signals';
-import { CustomInput } from "../../../common/custom-input/custom-input";
+import { Input } from "../../../common/forms/input/input";
 
 @Component({
   selector: 'account-form',
-  imports: [FormField, CustomInput],
+  imports: [FormField, Input],
   templateUrl: './account-form.html',
   styleUrl: './account-form.scss',
 })
