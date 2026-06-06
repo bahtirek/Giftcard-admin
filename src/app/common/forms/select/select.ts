e555d821636130c4b64d5ae532de9612bee97f0f
Bahti rek <bahtirek@gmail.com>
2026-06-06 17:02:44 -0400

    added logic for select and textarea

--- src/app/common/forms/select/select.ts
+++ src/app/common/forms/select/select.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { uniqueId } from '../../../helpers/uniqueid';
+import { FormField } from '@angular/forms/signals';
+import { NgClass } from '@angular/common';
+import { Option } from '../../../interfaces/options';
 
 @Component({
   selector: 'app-select',
-  imports: [],
+  imports: [FormField, NgClass],
   templateUrl: './select.html',
   styleUrl: './select.scss',
 })
-export class Select {}
+export class Select {
+  inputFormField = input<any>();
+  required = input<boolean>(false);
+  label = input<string>('');
+  placeholder = input<string>('');
+  type = input<string>('text');
+  id = signal<string>(uniqueId());
+  options = input<Option[]>([]);
+
+  isInvalid = signal<boolean>(false);
+}
--- src/app/common/forms/textarea/textarea.ts
+++ src/app/common/forms/textarea/textarea.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { uniqueId } from '../../../helpers/uniqueid';
+import { FormField } from '@angular/forms/signals';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-textarea',
-  imports: [],
+  imports: [FormField, NgClass],
   templateUrl: './textarea.html',
   styleUrl: './textarea.scss',
 })
-export class Textarea {}
+export class Textarea {
+  inputFormField = input<any>();
+  required = input<boolean>(false);
+  label = input<string>('');
+  placeholder = input<string>('');
+  rows = input<number>(3);
+  id = signal<string>(uniqueId());
+}
