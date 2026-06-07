cf1b4fe7bce621c04a91d03d0fbcd1418d120328
Bahti rek <bahtirek@gmail.com>
2026-06-06 21:38:32 -0400

    created boiler plate edit-acccount

---
+++ src/app/dashboard/account/edit-account/edit-account.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edit-account',
+  imports: [],
+  templateUrl: './edit-account.html',
+  styleUrl: './edit-account.scss',
+})
+export class EditAccount {}
